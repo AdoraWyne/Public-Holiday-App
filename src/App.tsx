4e85c8937e93226cf2d3636c02388156a9d225c1
AdoraWyne <dec_kwynez@hotmail.com>
2026-02-01 23:52:25 +1100

    Using useQuery to call API - also understand the API structure here. - the domain + "/Countries" + query param: "languageIsoCode=EN"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import React from "react"
+import React from "react";
 import './App.css';
+import { useQuery } from '@tanstack/react-query'
 import LearnReactQuery from "./Learn-React-Query/index"
 import LearnUseEffect from "./Learn-React-Query/indexWithUseEffect";
 
-const App = () => {
+const App: React.FC = () => {
+  const fetchCountries = async () => {
+    const url = "https://openholidaysapi.org/Countries?languageIsoCode=EN"
+    try {
+      const response = await fetch(url)
+      if(!response.ok) throw new Error(`Response status: ${response.status}`)
+
+      const result = await response.json()
+
+      return result;
+    } catch (error) {
+      if (error instanceof Error ){
+        console.error("Error msg: ", error.message)
+      } else {
+        console.error("Error msg: ", error)
+      }
+    }
+  }
+
+  const {data, isLoading, error} = useQuery({
+    queryKey: ["countries"],
+    queryFn: fetchCountries
+  })
+
   return (
-    <div className="main-app">
-    <LearnReactQuery />
-    <LearnUseEffect />
+    <div>
+      <h1>My App</h1>
+
+      <hr />
+      <div className="main-app">
+        <LearnReactQuery />
+        <LearnUseEffect />
+      </div>
     </div>
   )
 }
